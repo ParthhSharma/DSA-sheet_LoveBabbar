091c222fd4a70b0e566c2a5362afd863e13321e5
Parth Sharma <53979691+ParthhSharma@users.noreply.github.com>
2021-05-13 22:05:32 +0530

    Create ReverseAString.cpp

---
+++ ReverseAString.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    string str2;
+    while(str.length()>0){
+        str2.push_back(str.back());
+        str.pop_back();
+    }
+    return str2;
+}
